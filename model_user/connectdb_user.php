2af70a2d2f850621741495a3c0dc639553556289
Thai <lydangthaihung@gmail.com>
2024-09-20 11:10:46 +0700

    connect db

--- model_user/connectdb_user.php
+++ model_user/connectdb_user.php
@@ -1,17 +1,22 @@
 <?php
-    function connectdb(){
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
+function connectdb()
+{
+    $url = getenv('mysql://zd3kgzc76nc2zm9y:l3h85w10jgt63q1i@aqx5w9yc5brambgl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/o33qjulyf1xg03jd');
+    $dbparts = parse_url($url);
 
-        try {
-        $conn = new PDO("mysql:host=$servername;dbname=fashion", $username, $password);
+    $hostname = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $database = ltrim($dbparts['path'], '/');
+
+    try {
+        $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //echo "Connected successfully";
-        return $conn;
-        } catch(PDOException $e) {
+        echo "Connected successfully";
+        }
+    catch(PDOException $e)
+        {
         echo "Connection failed: " . $e->getMessage();
         }
-    }
-?>
+}
