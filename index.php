def3d67580fed1e07287c246487dd09a5fd2d286
Thai <lydangthaihung@gmail.com>
2024-09-20 10:45:37 +0700

    last index

--- index.php
+++ index.php
@@ -1,7 +1,2 @@
 <?php
-// Add Cowsay to Container
-$container->set(\Cowsayphp\AnimalInterface::class, function() {
-    return \Cowsayphp\Farm::create(\Cowsayphp\Farm\Cow::class);
-  });
-  
 header("Location: web_user/login_user.php");
