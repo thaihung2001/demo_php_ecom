52ce03485e17b371b9ce7a2654b81a59491f9cf1
Thai <lydangthaihung@gmail.com>
2024-09-20 10:42:52 +0700

    cowsay for /coolbeans

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
+// Add Cowsay to Container
+$container->set(\Cowsayphp\AnimalInterface::class, function() {
+    return \Cowsayphp\Farm::create(\Cowsayphp\Farm\Cow::class);
+  });
+  
 header("Location: web_user/login_user.php");
